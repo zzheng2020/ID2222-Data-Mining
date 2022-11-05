364310ab766c7647face19448605b12a1f9495fd
vicety <335462631@qq.com>
2022-11-06 03:03:35 +0800

    update Shingling class for the assignment requires the output to be ordered

--- Homework1/shingling.py
+++ Homework1/shingling.py
@@ -11,14 +11,14 @@ in the form of an ordered set of its hashed k-shingles.
 class Shingling():
     def __init__(self) -> None:
         return None
-        
+
     def shingling_hash(self, shingling: str):
         return hash(shingling)
-    
+
     def shingling(self, docs: str, k=9):
         docs_len = len(docs)
         shingling_set = set()
         for i in range(docs_len - k + 1):
             hash_result = self.shingling_hash(docs[i:i+k])
             shingling_set.add(hash_result)
-        return shingling_set
+        return sorted(shingling_set)
