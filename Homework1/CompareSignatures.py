f5b78f676b4737ab83cdc072698266cb4613213c
vicety <335462631@qq.com>
2022-11-06 03:15:21 +0800

    add stub for MinHashing and CompareSignatures

---
+++ Homework1/CompareSignatures.py
@@ -0,0 +1,15 @@
+"""
+Authors: Chengyang Huang, Ziheng Zhang
+Course:  ID2222 Data Mining
+"""
+
+"""
+Compute similarity between two documents using minHash signatures.
+"""
+
+from typing import List
+
+
+class CompareSignatures:
+    def similarity(self, sig1: List[int], sig2: List[int]) -> float:
+        pass
---
+++ Homework1/minhash.py
@@ -0,0 +1,15 @@
+"""
+Authors: Chengyang Huang, Ziheng Zhang
+Course:  ID2222 Data Mining
+"""
+
+"""
+Compute minHash signatures from one ordered hashed shingling.
+"""
+
+from typing import List
+
+
+class MinHashing:
+    def buildMinHash(self, hashedShingling: List[int], nHash: int) -> List[int]:
+        pass
